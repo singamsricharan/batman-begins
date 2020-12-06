30eb4c399687a5dad5d8be5058595f3f55119e8d
sricharan <singam.sricharan1603@gmail.com>
2020-12-06 20:16:16 +0530

    changes

--- proc41-template-master/Drops.js
+++ proc41-template-master/Drops.js
@@ -1,21 +1,24 @@
 class Drop{
-    constructor(x,y){
-      var options={
-        isStatic:false,
-        friction:0.1
+  constructor(x,y){
+      var options = {
+          friction: 0.1,
+          restitution:0.1           
       }
-      this.drop=Bodies.circle(x,y,10,options);
-      World.add(world,this.drop);
-      Matter.Body.setMass(this.drop,0.00000009)
-      this.x=x;
-      this.y=x;
-    }
-    disp(){
-      if(this.drop.position.y>=height){
-        Matter.Body.setPosition(this.drop, {x:random(0,500),y:random(-500,0)})
-      }
-      fill("blue");
-      ellipseMode(RADIUS);
-      ellipse(this.drop.position.x,this.drop.position.y,5,5);
-    }
+      this.rain = Bodies.circle(x,y,5,options)
+      this.radius = 4.5;
+      World.add(world, this.rain);
   }
+
+  updateY(){     
+      if(this.rain.position.y > height){
+
+          Matter.Body.setPosition(this.rain, {x:random(0,400), y:random(0,400)})
+      }
+  }
+
+  showDrop(){
+      fill(16, 134, 245)
+      ellipseMode(CENTER);
+      ellipse(this.rain.position.x,this.rain.position.y,this.radius,this.radius);
+  }
+}
